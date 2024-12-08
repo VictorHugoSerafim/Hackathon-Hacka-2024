315f2e161fbba42e556bd0f331f68c3349e94e0f
Victor Hugo <victor.hugo.serafim@hotmail.com>
2024-12-08 06:01:56 -0300

    Configuração do ambiente completo de desenvolvimento Dockerizado com React, Django, PostgreSQL e pgAdmin.

---
+++ db/init.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE mydatabase;
+CREATE USER postgres WITH PASSWORD 'example';
+ALTER ROLE postgres SET client_encoding TO 'utf8';
+ALTER ROLE postgres SET default_transaction_isolation TO 'read committed';
+ALTER ROLE postgres SET timezone TO 'UTC';
+GRANT ALL PRIVILEGES ON DATABASE mydatabase TO postgres;
